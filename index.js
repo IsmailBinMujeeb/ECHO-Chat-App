2993b5f555a72f4a39a085fcda6035af5489cc20
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-04-02 10:55:16 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import { config } from "dotenv";
 config();
 
 // Routes Import
 import indexRouter from "./routes/home.router.js"
+import userRouter from "./routes/user.routes.js"
 
 const app = express();
 const PORT  = process.env.PORT;
@@ -13,8 +15,10 @@ app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(cookieParser());
 
 app.use('/', indexRouter);
+app.use('/user/', userRouter);
 
 export default app
