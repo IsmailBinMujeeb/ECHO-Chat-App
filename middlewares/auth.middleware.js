7725685028fc7d7d4930b12bb34a2c8286d1a0d9
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-04-02 10:58:22 +0530

    add auth middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import userModel from "../models/user.model.js";
+import ApiError from "../utils/ApiError.js";
+
+export const authenticate = async (req, _, next) => {
+
+    const { ACCESS_TOKEN } =  req.cookies;
+
+    const decodedAccessToken = jwt.verify(ACCESS_TOKEN, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await userModel.findById(decodedAccessToken?.id).select("-password -refreshToken");
+
+    if (!user) throw new ApiError(401, 'Unauthorized request');
+
+    req.user = user;
+    next();
+}
