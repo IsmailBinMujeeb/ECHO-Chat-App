9702872a61c56a98210af0744ac0f67d80ee61bf
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-04-02 10:57:33 +0530

    update user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-import { hashPasswordMongooseMiddleware } from "../middlewares/password.mongoose.middleware.js";
+import { hashPasswordMongooseMiddleware, comparePasswordMongooseMiddleware } from "../middlewares/password.mongoose.middleware.js";
+import jwt from "jsonwebtoken"
 
 const userSchema = new mongoose.Schema({
 
@@ -8,7 +9,7 @@ const userSchema = new mongoose.Schema({
         require: [true, 'userhandle is required in user model'],
         unique: [true, 'userhandle must be unique in user model'],
         validate: {
-            validator: function(value){
+            validator: function (value) {
                 return /^(?!bot)(?!.*bot$)[a-zA-Z0-9_]{3,20}$/.test(value);
             },
             message: props => `${props.value} is not a valid userhandle`
@@ -53,10 +54,25 @@ const userSchema = new mongoose.Schema({
         default: [],
     }],
 
+    refreshToken: {
+        type: String,
+        default: '',
+    }
+
 }, { timestamps: true });
 
-userSchema.pre('save',  hashPasswordMongooseMiddleware);
+userSchema.pre('save', hashPasswordMongooseMiddleware);
 
-// TODO: Add Methods for JWT and comparing password
+userSchema.method('comparePassword', comparePasswordMongooseMiddleware);
+
+userSchema.method('generateAuthTokens', function () {
+    const ACCESS_TOKEN = jwt.sign({ id: this._id, userhandle: this.userhandle, profile: this.profile }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: process.env.ACCESS_TOKEN_EXPIRES_AT });
+    const REFRESH_TOKEN = jwt.sign({ id: this._id }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: process.env.REFRESH_TOKEN_EXPIRES_AT });
+
+    this.refreshToken = REFRESH_TOKEN;
+    this.save()
+
+    return { ACCESS_TOKEN, REFRESH_TOKEN }
+})
 
 export default mongoose.model('user', userSchema);
