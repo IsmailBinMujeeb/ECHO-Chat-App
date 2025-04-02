6ede971d90331261af172b00c83523c9394b81c0
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-04-02 10:58:02 +0530

    update password mongoose middleware

--- middlewares/password.mongoose.middleware.js
+++ middlewares/password.mongoose.middleware.js
@@ -6,3 +6,8 @@ export const hashPasswordMongooseMiddleware = async function (next){
 
     this.password = await bcrypt.hash(this.password, 10)
 }
+
+export const comparePasswordMongooseMiddleware = async function(password){
+
+    return await bcrypt.compare(password, this.password);
+}
