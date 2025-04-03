6b37cb24d92f645671ef9b1f02f29bb9fe78b702
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-04-03 20:06:56 +0530

    update server js

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import app from "./index.js";
 import conn from "./config/db.config.js";
+import socketHandler from "./socket/socketHandler.js";
+import { createServer } from "http";
 
 (
     async () => {
 
         await conn() // Connect to Database
-        app.listen(app.get('PORT')); // Start server at port $PORT
+
+        const server = createServer(app);
+        socketHandler(server);
+
+        server.listen(app.get('PORT')); // Start server at port $PORT
     }
 )();
