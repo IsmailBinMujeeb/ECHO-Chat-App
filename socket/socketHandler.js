42f5947fea0f9936b9579f67170d55261de42b43
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-04-04 19:35:55 +0530

    update socketHandler

--- socket/socketHandler.js
+++ socket/socketHandler.js
@@ -7,40 +7,46 @@ const socketHandler = (server) => {
 
     io.on('connection', (socket) => {
 
-        socket.on('join-chat',async ({ chatId, user })=>{
+        socket.on('join-chat', async ({ chatId, user }) => {
 
             await socket.join(chatId);
-            
+
             socket.data.roomId = chatId
             io.emit("user-connect", { user });
 
-            socket.on("im-online", ({ user })=>{
-                
-                socket.broadcast.emit("already-online", {user})
+            socket.on("im-online", ({ user }) => {
+
+                socket.broadcast.emit("already-online", { user })
             })
         });
 
-        socket.on("emit-message", async ({ message, senderId, recieverId, chatId }) => {
+        socket.on("emit-message", async ({ message, senderId, recieverId, chatId, replyToMessageId }) => {
 
             try {
-                
-                await messageModel.create({
-                    senderId,
-                    recieverId,
-                    chatId,
-                    content: message,
-                });
 
-                console.log(chatId)
+                const replyingMessage = await messageModel.findById(replyToMessageId);
+
+                if (replyingMessage) {
+                    const newMessage = await messageModel.create({
+                        senderId,
+                        recieverId,
+                        chatId,
+                        replyTo: replyToMessageId || null,
+                        content: message,
+                    });
+                } else {
+                    const newMessage = await messageModel.create({
+                        senderId,
+                        recieverId,
+                        chatId,
+                        content: message,
+                    });
+                }
 
                 socket.to(socket.data.roomId).emit("message-recieved", { message });
             } catch (error) {
                 throw new Error(error.message)
             }
-        })
-
-        socket.on('disconnect', ()=>{
-            
         })
     })
 }
