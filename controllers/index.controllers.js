9c9bfd2109250b027b802ff2ffb1c83c1cd4c0e4
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-04-06 20:48:39 +0530

    update index controller

--- controllers/index.controllers.js
+++ controllers/index.controllers.js
@@ -1,4 +1,18 @@
-export const indexController = (req, res)=>{
+import userModel from "../models/user.model.js";
+import ApiError from "../utils/ApiError.js";
+import jwt from 'jsonwebtoken'
 
-    return res.render('home_page');
+export const indexController = async (req, res)=>{
+
+    const { ACCESS_TOKEN } = req.cookies;
+
+    if (!ACCESS_TOKEN) throw new ApiError(401, 'Unauhterized request');
+
+    const decodedToken = jwt.verify(ACCESS_TOKEN, process.env.ACCESS_TOKEN_SECRET);
+
+    if (!decodedToken) throw new ApiError(401, 'token expired');
+
+    const user = await userModel.findById(decodedToken.id).select('-password -refreshToken');
+
+    return res.render('home_page', {chats: null, user});
 }
