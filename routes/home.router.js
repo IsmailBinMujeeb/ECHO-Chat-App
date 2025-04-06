0e43a4b28e2c8c96969f50d42771fd062ca36c39
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-04-06 20:50:04 +0530

    update home routes

--- routes/home.router.js
+++ routes/home.router.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { indexController } from "../controllers/index.controllers.js";
+import { authenticate } from "../middlewares/auth.middleware.js";
+import asyncHandler from "../utils/asyncHandler.js"
 
 const router = Router();
 
-router.get('/', indexController);
+router.get('/', asyncHandler(authenticate), asyncHandler(indexController));
 
 export default router;
