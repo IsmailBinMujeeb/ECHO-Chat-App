756c0c94a17ac212e1fad7c5597d62f7a741daab
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-04-14 08:38:57 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { config } from "dotenv";
 config();
 
@@ -7,6 +8,7 @@ config();
 import indexRouter from "./routes/home.router.js"
 import userRouter from "./routes/user.routes.js"
 import chatRouter from "./routes/chat.routes.js"
+import messageRouter from "./routes/api/v1/message.routes.js"
 
 const app = express();
 const PORT  = process.env.PORT;
@@ -18,9 +20,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(cookieParser());
+app.use(cors({
+    origin: process.env.CORS,
+    credentials: true,
+}))
 
 app.use('/', indexRouter);
 app.use('/user/', userRouter);
 app.use('/chat/', chatRouter);
+app.use('/api/', messageRouter);
 
 export default app
